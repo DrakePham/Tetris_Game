95464824f0ed2340d87a2ca9ff3a668230b44f58
DrakePham <89468358+DrakePham@users.noreply.github.com>
2024-09-11 14:23:49 -0500

    grid initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,30 @@
 #include <raylib.h>
-#include "ball.h"
+#include "grid.h"
 
 int main()
 {
-    InitWindow(300, 600, "Tetris Game");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
+    
+    try{
+        InitWindow(300, 600, "Tetris Game");
+        Color darkBlue = {0, 121, 241, 255};
        
+        SetTargetFPS(60);
 
-        BeginDrawing();
+        Grid grid = Grid();
+        grid.printGrid();
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(darkBlue);
+            EndDrawing();
+        }
         
-        
-        EndDrawing();
+        CloseWindow();
+    }catch(const char* msg){
+        std::cerr << msg << std::endl;
     }
-
-    CloseWindow();
+    
     return 0;
+
+
 }
