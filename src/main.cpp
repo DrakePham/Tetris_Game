055d579a98da7c3299a15c237e47c09158310839
DrakePham <89468358+DrakePham@users.noreply.github.com>
2024-09-12 13:57:52 -0500

    host locally sucessfully

--- src/main.cpp
+++ src/main.cpp
@@ -33,3 +33,32 @@ int main()
 
 
 }
+
+
+// emcc -o snake.html main.cpp -Wall -std=c++14 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os \
+// -I /Users/thongpham/Downloads/raylib/src -I /Users/thongpham/Downloads/raylib/src/external \
+// -L /Users/thongpham/Downloads/raylib/src -s USE_GLFW=3 -s ASYNCIFY \
+// -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --preload-file Graphics --preload-file Sounds \
+// --shell-file /Users/thongpham/Downloads/raylib/src/shell.html \
+// -DPLATFORM_WEB -s 'EXPORTED_FUNCTIONS=["_free","_malloc","_main"]' \
+// -s EXPORTED_RUNTIME_METHODS=ccall
+
+// emcc src/*.cpp -o tetris.html -Wall -std=c++14 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os \
+// -I /Users/thongpham/Downloads/raylib/src -I /Users/thongpham/Downloads/raylib/src/external \
+// -L /Users/thongpham/Downloads/raylib/src/web -s USE_GLFW=3 -s ASYNCIFY \
+// -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 \
+// --shell-file /Users/thongpham/Downloads/raylib/src/shell.html \
+// -DPLATFORM_WEB -s 'EXPORTED_FUNCTIONS=["_free","_malloc","_main"]' \
+// -s EXPORTED_RUNTIME_METHODS=ccall \
+// /Users/thongpham/Downloads/raylib/src/web/libraylib.a -s USE_GLFW=3 -DPLATFORM_WEB
+
+// emcc src/*.cpp -o /web/tetris.html -Wall -std=c++14 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os \
+// -I /Users/thongpham/Downloads/raylib/src -I /Users/thongpham/Downloads/raylib/src/external \
+// -L /Users/thongpham/Downloads/raylib/src/web -s USE_GLFW=3 -s ASYNCIFY \
+// -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 \
+// --shell-file /Users/thongpham/Downloads/raylib/src/shell.html \
+// -DPLATFORM_WEB -s 'EXPORTED_FUNCTIONS=["_free","_malloc","_main"]' \
+// -s EXPORTED_RUNTIME_METHODS=ccall \
+// /Users/thongpham/Downloads/raylib/src/web/libraylib.a -s USE_GLFW=3 -DPLATFORM_WEB
+
+// python3 -m http.server 8000
